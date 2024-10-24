af3140a9de1c9a395b70d933bb6921aadbdd87fa
sharatyaragatti <sharatyaragatti@credenceanalytics.com>
2024-10-24 17:46:03 +0530

    some formatting

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
-const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config();
 const path = require("path");
 const dns = require("dns");
+
+dotenv.config();
+const app = express();
 const connectString = process.env.URL;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -14,7 +16,6 @@ const urlSchema = new mongoose.Schema({
   shortUrl: String,
   longUrl: String,
 });
-
 const Url = mongoose.model("Url", urlSchema);
 
 app.get("/", (req, res) => {
@@ -23,37 +24,51 @@ app.get("/", (req, res) => {
 
 app.post("/api/shorturl", async (req, res) => {
   const longUrl = req.body.url;
-  const urlRegex = /^(https?:\/\/)[\w.-]+\.(com)$/;
+
+  const urlRegex = /^(https?:\/\/)/;
   if (!urlRegex.test(longUrl)) {
-    res.send({ error: "invalid url" });
+    console.log("Invalid URL format:", longUrl);
+    return res.json({ error: "Invalid URL" });
   }
+
   const hostname = new URL(longUrl).hostname;
 
   dns.lookup(hostname, async (err) => {
     if (err) {
-      return res.json({ error: "invalid url" });
+      console.log("DNS Lookup failed:", err);
+      return res.json({ error: "Invalid URL" });
     }
+
     const shortUrl = Math.floor(Math.random() * 10000).toString();
-    const urlData = new Url({
-      longUrl: longUrl,
-      shortUrl: shortUrl,
-    });
+    
+    const urlData = new Url({ longUrl, shortUrl });
     await urlData.save();
+
     res.json({ original_url: longUrl, short_url: Number(shortUrl) });
   });
 });
-app.get(`/api/shorturl/:shortUrl`, async (req, res) => {
-  const shorturl = req.params.shortUrl;
-  const urlData = await Url.findOne({ shortUrl: "" + shorturl });
+
+app.get("/api/shorturl/:shortUrl", async (req, res) => {
+  const shortUrl = req.params.shortUrl;
+
+  const urlData = await Url.findOne({ shortUrl: shortUrl });
+  
   if (urlData) {
     return res.redirect(urlData.longUrl);
-  } else res.json({ error: "post a url please" });
+  } else {
+    console.log("No URL found for:", shortUrl);
+    return res.json({ error: "No short URL found" });
+  }
 });
 
-mongoose.connect(connectString).then(() => {
-  app.listen(3000, () => {
-    console.log("Connected to MongoDB and listening on port 3000");
+mongoose.connect(connectString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Connected to MongoDB and listening on port 3000");
+    });
+  })
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
   });
-});
 
 module.exports = app;
