b6ba8b7e4bb965b82fad2114c47a04ccdab65582
sharatyaragatti <sharatyaragatti@credenceanalytics.com>
2024-10-24 15:53:34 +0530

    redirection achieved

--- index.js
+++ index.js
@@ -9,16 +9,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+var shortUrl = "";
+var longUrl = "";
 app.post("/api/shorturl", (req, res) => {
-  const longUrl = req.body.url;
+  longUrl = req.body.url;
   const urlRegex = /^(https?:\/\/)[\w.-]+\.(com)$/;
   if (!urlRegex.test(longUrl)) {
     res.send({ error: "Invalid URL" });
   }
-  const shortUrl = Math.floor(Math.random() * 10000);
+  shortUrl = Math.floor(Math.random() * 10000);
   res.json({ original_url: longUrl, short_url: shortUrl });
 });
-
+app.get(`/api/shorturl/${shortUrl}`,(req,res)=>{
+    res.redirect(`${longUrl}`);
+});
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
